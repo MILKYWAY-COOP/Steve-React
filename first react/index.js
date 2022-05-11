68e6d87c6192cf6aaa9676b7a37138dbd8519af2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-11 17:23:32 +0300

    Inline React

---
+++ first react/index.js
@@ -0,0 +1,11 @@
+ReactDOM.render(<h1>Hello Everyone</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+        <p>Hi my name is Steve</p>
+        <ul>
+            <li>Football</li>
+            <li>Coding</li>
+        </ul>
+  </div>,
+  document.getElementById('root')
+);
