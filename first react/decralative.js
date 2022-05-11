c875b55ec7bc45cfd9824b063ef3a5c50c07f6c2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-11 17:57:27 +0300

    React is Decralative

---
+++ first react/decralative.js
@@ -0,0 +1,16 @@
+ReactDOM.render(<h1 className='header'>Hello, React!</h1>, document.getElementById("root"))
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+// let newElement = document.createElement("h1")
+// newElement.innerText = "Testing out Vanilla JavaScript" 
+// newElement.className = "header"
+// document.getElementById("root").append(newElement)
