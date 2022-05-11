3631721fcee00c99b5e0ec9fd237413eea43d209
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-11 18:06:42 +0300

    Working with JSX

---
+++ first react/jsx.js
@@ -0,0 +1,11 @@
+// let newElement = document.createElement("h1")
+// newElement.innerText = "This is JSX"
+// newElement.className = "header"
+// document.getElementById("root").append(newElement)
+
+// console.log(newElement)
+
+ReactDOM.render(
+  <h1 className="header">Hello, React!</h1>,
+  document.getElementById('root')
+);
