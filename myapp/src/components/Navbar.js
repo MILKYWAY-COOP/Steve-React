006b0023d5e26e8ac1ddd26ab171b1403e4a5625
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-12 16:17:57 +0300

    Added a Background Image

--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import reactLogo from './images/react-icon-small.png';
+import reactLogo from './images/react-icon-small.svg';
 
 export default function Navbar() {
   return (
     <nav>
-      <img src={ reactLogo} height="80px"/>
+      
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
