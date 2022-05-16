64915f3ccb556a92249453a8ee01f27afeb63db5
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-16 12:15:16 +0300

    Working on the navBar

---
+++ airbnb/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import img from './images/airbnb.svg'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={img} alt='air bnb logo'/>
+        </nav>
+    )
+}
