4ed9fb70764fa9eba85a74cf20e589ef50b67efc
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-17 12:30:36 +0300

    Working with Props

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import Card from './components/Card'
+import Ngong from './components/images/ngong.jpg'
 
 /*
 Challenge: Build the Navbar component.
@@ -12,7 +13,14 @@ export default function App() {
     return (
         <>
             <Navbar />
-            <Card />
+            <Card
+                img = {Ngong}
+                rating={5.0}
+                viewCount={6}
+                country="Kenya"
+                title="Visit Ngong with Stephen Muchendu"
+                price= {100}
+            />
         </>
     )
 }
