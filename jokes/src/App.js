b5ecf59ce5407402a973ba9eb2fd4f3b7d30f680
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-17 13:15:43 +0300

    Working with external JS data

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -3,12 +3,10 @@ import Joke from './components/Joke';
 import jokesData from './components/jokesData';
 
 export default function App() {
-  const jokeElement = jokesData.map(joke => {
-    return <Joke setup={joke.setup} punchline={ joke.punchline}/>
-  })
-  return <div>
-    {jokeElement}
-  </div>;
+  const jokeElement = jokesData.map((joke) => {
+    return <Joke setup={joke.setup} punchline={joke.punchline} />;
+  });
+  return <div>{jokeElement}</div>;
 }
 
 {
