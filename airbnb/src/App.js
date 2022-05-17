2a157de1390cd241b77933c43aa7c14d4a4fd11b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-17 13:26:54 +0300

    Updated code to read from the public directory

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,25 +2,19 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import Card from './components/Card'
-import Ngong from './components/images/ngong.jpg'
-
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
 
 export default function App() {
     return (
-        <>
-            <Navbar />
-            <Card
-                img = {Ngong}
-                rating={5.0}
-                viewCount={6}
-                country="Kenya"
-                title="Visit Ngong with Stephen Muchendu"
-                price= {100}
-            />
-        </>
-    )
+      <>
+        <Navbar />
+        <Card
+          img="ngong.jpg"
+          rating={5.0}
+          viewCount={6}
+          country="Kenya"
+          title="Visit Ngong with Stephen Muchendu"
+          price={100}
+        />
+      </>
+    );
 }
--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -5,7 +5,7 @@ import Star from './images/star.svg'
 export default function Card(props) {
     return (
       <div className="card">
-        <img src={props.img} alt="Ngong hills image" className="card--image" />
+        <img src={`../images/${props.img}`} alt="Ngong hills image" className="card--image" />
         <div className="card-stats">
           <img src={Star} className="card--star" />
           <span>{ props.rating}</span>
