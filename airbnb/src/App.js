dfac5573293e62d4fa15c785708244fd72d94d65
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-18 12:15:05 +0300

    Modified props

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -4,31 +4,28 @@ import Hero from './components/Hero';
 import Card from './components/Card';
 import Data from './data';
 
-
 export default function App() {
-
   const data = Data.map((item) => {
     return (
-        <Card
+      <Card
         key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        viewCount={item.stats.reviewCount}
-        location={item.location}
-        price={item.price}
-        title={item.title}
-        openSpots={item.openSpots}
+        item={item}
+        //{...item}
+        // img={item.coverImg}
+        // rating={item.stats.rating}
+        // viewCount={item.stats.reviewCount}
+        // location={item.location}
+        // price={item.price}
+        // title={item.title}
+        // openSpots={item.openSpots}
       />
     );
   });
-    
-    
   return (
     <>
       <Navbar />
-          <section className='cards-list'>
-              {data}
-      </section>
+      <Hero />
+      <section className="cards-list">{data}</section>
     </>
   );
 }
--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import Star from './images/star.svg'
-
+import Star from './images/star.svg';
 
 export default function Card(props) {
-  let badgeText
-  if (props.openSpots === 0) {
-    badgeText = "Sold Out"
-  } else if (props.location === "Online") {
-    badgeText = "Online"
+  console.log(props.item);
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = 'Sold Out';
+  } else if (props.item.location === 'Online') {
+    badgeText = 'Online';
   }
 
-  console.log(props.openSpots)
-    return (
-      <div className="card">
-        {badgeText && <div className='card--badge'>{ badgeText}</div>}
-        <img src={`../images/${props.img}`} className="card--image" />
-        <div className="card-stats">
-          <img src={Star} className="card--star" />
-          <span>{props.rating}</span>
-          <span className="gray">({props.viewCount}) • </span>
-          <span className="gray">{props.location}</span>
-        </div>
-        <p className="card--title">{props.title}</p>
-        <p className="card--price">
-          <span className="bold">From ${props.price}</span>
-        </p>
+  console.log(props.openSpots);
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={`../images/${props.item.coverImg}`} className="card--image" />
+      <div className="card-stats">
+        <img src={Star} className="card--star" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
       </div>
-    );
+      <p className="card--title">{props.item.title}</p>
+      <p className="card--price">
+        <span className="bold">From ${props.item.price}</span>
+      </p>
+    </div>
+  );
 }
