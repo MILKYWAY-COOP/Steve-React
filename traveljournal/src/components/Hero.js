2b7f1b98a6cbfd17882dd8253ae0b038e9156ec7
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-18 16:44:22 +0300

    Updates

--- traveljournal/src/components/Hero.js
+++ traveljournal/src/components/Hero.js
@@ -1,30 +1,30 @@
-import React from "react";
-import Icon from "./images/icon.png"
+import React from 'react';
+import Icon from './images/icon.png';
 
 export default function Hero(props) {
-    return (
-      <div className="card--body">
-        <div className="card">
-          <img src={`../images/${props.item.imageUrl}`}  className="card--img" />
-          <div className="card--info">
-            <div className="card--location">
-              <img src={Icon} className="icon" />
-              <h6 className="location--country">{props.item.location}</h6>
-              <a
-                href={props.item.googleMapsUrl}
-                target="_blank"
-                className="location--map"
-              >
-                View on Google Maps
-              </a>
-            </div>
-            <h4 className="card--place">{props.item.title}</h4>
-            <h5 className="card--date">
-              {props.item.startDate} - {props.item.endDate}
-            </h5>
-            <p className="card--description">{props.item.description}</p>
+  return (
+    <div className="card--body">
+      <div className="card">
+        <img src={`../images/${props.item.imageUrl}`} className="card--img" />
+        <div className="card--info">
+          <div className="card--location">
+            <img src={Icon} className="icon" />
+            <h6 className="location--country">{props.item.location}</h6>
+            <a
+              href={props.item.googleMapsUrl}
+              target="_blank"
+              className="location--map"
+            >
+              View on Google Maps
+            </a>
           </div>
+          <h4 className="card--place">{props.item.title}</h4>
+          <h5 className="card--date">
+            {props.item.startDate} - {props.item.endDate}
+          </h5>
+          <p className="card--description">{props.item.description}</p>
         </div>
       </div>
-    );
+    </div>
+  );
 }
