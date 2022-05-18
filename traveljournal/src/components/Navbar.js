8eec64a912b46c127049fe1606ffdbbe94de1cc4
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-18 21:43:02 +0300

    Added box shadow feature

--- traveljournal/src/components/Navbar.js
+++ traveljournal/src/components/Navbar.js
@@ -1,11 +1,11 @@
-import React from "react"
-import img from './images/Africa.png'
+import React from 'react';
+import img from './images/Africa.png';
 
 export default function NavBar() {
     return (
-        <nav>
-            <img src={img} className='nav-logo' />
-            <p className='nav-text'>My travel journal</p>
+        <nav id="navbar">
+            <img src={img} className="nav-logo" />
+            <p className="nav-text">My travel journal</p>
         </nav>
-    )
+    );
 }
