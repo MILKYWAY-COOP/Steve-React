fcc38cb892d260757ed83ce441bcd4b819d0d2a6
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-19 14:12:50 +0300

    React states incoming

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import memeData from '../memeData';
 
 export default function MemeComponent() {
+    function getMemeImage() {
+        const memesArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
   return (
     <main>
       <div className='form'>
         <input type="text"className='form--input'placeholder='top text'/>
         <input type="text"className='form--input'placeholder='bottom text'/>
-        <button className='form--button'>Get a new meme image 🖼</button>
+        <button className='form--button' onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
     </main>
   );
