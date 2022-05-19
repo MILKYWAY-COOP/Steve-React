1f016f233c3b0adfccaa2ac9f3b2e818c9a3736f
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-19 12:26:23 +0300

    Meme Data API

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function MemeComponent() {
   return (
     <main>
-      <form className='form'>
+      <div className='form'>
         <input type="text"className='form--input'placeholder='top text'/>
         <input type="text"className='form--input'placeholder='bottom text'/>
         <button className='form--button'>Get a new meme image 🖼</button>
-      </form>
+      </div>
     </main>
   );
 }
