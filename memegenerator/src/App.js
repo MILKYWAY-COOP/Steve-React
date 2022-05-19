938a02095edd33987ead96f4ab7ca7840267b4ad
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-19 11:58:31 +0300

    Meme Inputs

--- memegenerator/src/App.js
+++ memegenerator/src/App.js
@@ -1,10 +1,12 @@
 import react from 'react'
 import NavBar from './components/Navbar'
+import MemeComponent from './components/Meme'
 
 export default function App() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      <MemeComponent />
     </>
   )
 }
