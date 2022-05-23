e3af09eafd87cd23b693de73d70230e557f2a2b8
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-23 09:51:03 +0300

    Aluta continua

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -2,18 +2,20 @@ import React from 'react';
 import memeData from '../memeData';
 
 export default function MemeComponent() {
-    function getMemeImage() {
-        const memesArray = memeData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        console.log(url)
-    }
+  function getMemeImage() {
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
   return (
     <main>
-      <div className='form'>
-        <input type="text"className='form--input'placeholder='top text'/>
-        <input type="text"className='form--input'placeholder='bottom text'/>
-        <button className='form--button' onClick={getMemeImage}>Get a new meme image 🖼</button>
+      <div className="form">
+        <input type="text" className="form--input" placeholder="top text" />
+        <input type="text" className="form--input" placeholder="bottom text" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </div>
     </main>
   );
