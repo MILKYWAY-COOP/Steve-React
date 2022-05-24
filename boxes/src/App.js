661a98382cbce440c9e303e9dcfabea6ec44b537
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-24 15:03:09 +0300

    Starter Files

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import box from './components/box'
 
-function App() {
+export default function App(props) {
+  const [squares, setSquares] = React.useState(box)
+
+  const squaresElements = squares.map(square => (
+    <div className='box' key={square.id}></div>
+  ))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      {squaresElements }
+    </main>
+  )
 }
-
-export default App;
