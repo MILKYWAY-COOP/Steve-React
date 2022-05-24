244e96468a92acb627c16c87dc241457c5495c62
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-24 15:11:07 +0300

    Working with Dynamic Styles

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -4,8 +4,12 @@ import box from './components/box'
 export default function App(props) {
   const [squares, setSquares] = React.useState(box)
 
+  const styles = {
+    backgroundColor: props.darkMode ? '#222222' : '#cccccc'
+  }
+
   const squaresElements = squares.map(square => (
-    <div className='box' key={square.id}></div>
+    <div style={styles} className='box' key={square.id}></div>
   ))
   return (
     <main>
