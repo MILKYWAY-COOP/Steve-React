7dbc0a40e5baef4c64f84c40c48f11bb8c00442a
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-24 16:32:45 +0300

    Static State

--- state/src/App.js
+++ state/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 
 export default function App() {
-  const result = React.useState("Hello")
+  const [isImportant, setIsImportant] = React.useState("Yes")
+
+  function handleClick() {
+    setIsImportant("No")
+  }
+
   return (
-    <div className='state'>
-      <h1 className='state--title'> Is state important to Know?</h1>
-      <div className='state--value'>
-        <h1>{ result[0]}</h1>
+    <div className="state">
+      <h1 className="state--title"> Is state important to Know?</h1>
+      <div onClick={handleClick} className="state--value">
+        <h1>{isImportant}</h1>
       </div>
     </div>
-  )
+  );
 }
