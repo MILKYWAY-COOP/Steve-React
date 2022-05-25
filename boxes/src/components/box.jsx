070378e0f2ff62113c3e5dcd188c02dcc20a6fd7
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-25 11:03:06 +0300

    Man I'm good

--- boxes/src/components/box.jsx
+++ boxes/src/components/box.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default function Boxy(props) {
+  const [isOn, setIsOn] = React.useState(props.isOn);
+
+  function toggle() {
+    setIsOn(!isOn);
+  }
   const styles = {
-    backgroundColor: props.isOn ? '#36454F' : '#ebf2f2'
+    backgroundColor: isOn ? '#36454F' : '#ebf2f2'
   };
-  return <div
-    style={styles}
-    className="box"></div>;
+  return <div style={styles} className="box" onClick={toggle}></div>;
 }
