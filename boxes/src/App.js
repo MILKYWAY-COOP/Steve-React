d68f5bd48a6f75b8f0355e7efe7f28b68dd999be
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-25 10:25:30 +0300

    Backtracking

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,44 +1,16 @@
-import React from 'react'
+import React from 'react';
 import boxes from './components/boxes'
-import Box from './components/box'
 
-export default function App(props) {
-  const [squares, setSquares] = React.useState(boxes)
+export default function App() {
+  const [boîte, setboîte] = React.useState(boxes); 
 
-  function toggle(id) {
-     setSquares((prevSquares) => {
-       const newSquares = [];
-       for (let i = 0; i < prevSquares.length; i++) {
-         const currentSquare = prevSquares[i];
-         if (currentSquare.id === id) {
-           const updatedSquare = {
-             ...currentSquare,
-             on: !currentSquare.on
-           };
-           newSquares.push(updatedSquare);
-         } else {
-           newSquares.push(currentSquare);
-         }
-       }
-       return newSquares;
-     });
-  }
-  
-  const styles = {
-    backgroundColor: props.darkMode ? '#222222' : '#cccccc'
-  }
-
-  const squaresElements = squares.map((square) => (
-    <Box
-      key={square.id}
-      id={square.id}
-      on={square.on}
-      toggle={toggle} />
-  ));
+  const boîteElements = boîte.map((item) => {
+    return <div key={item.id} className="box"></div>;
+  });
 
   return (
     <main>
-      {squaresElements }
+      {boîteElements}
     </main>
-  )
+  );
 }
