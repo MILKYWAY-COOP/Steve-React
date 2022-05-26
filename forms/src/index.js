3260a530da086c7b9d0896a6c049f7ecc31dc0af
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-26 10:56:14 +0300

    Working on a sign up form

--- forms/src/index.js
+++ forms/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Form from './Form';
 import ControlledInputs from './ControlledInputs';
+import SignUp from './Signup';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <ControlledInputs />
+    <SignUp />
   </>
 );
