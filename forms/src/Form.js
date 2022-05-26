b203af532f9c354c17f61e082704a79cec3ed407
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-26 08:48:15 +0300

    More forms

--- forms/src/Form.js
+++ forms/src/Form.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
 export default function Form() {
-    
+    const [firstName, setFirstName] = React.useState('')
+    const [lastName, setLastName] = React.useState('')
+
+    function handleChangeFName(event) { 
+        setFirstName(event.target.value);
+    }
+
+    function handleChangeLName(event) {
+      setLastName(event.target.value);
+    }
+
+    console.log(firstName, lastName);
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="First Name"
+          onChange={handleChangeFName}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={handleChangeLName}
+        />
+      </form>
+    );
 }
