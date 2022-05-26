d03d87966b8c75d3fa941d63e9172900888fc3ac
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-26 10:14:47 +0300

    Controlled Components

--- forms/src/ControlledInputs.js
+++ forms/src/ControlledInputs.js
@@ -5,19 +5,22 @@ export default function ControlledInputs() {
     firstName: '',
     lastName: '',
     email: '',
-    comments: ''
+    comments: '',
+    isFriendly: false,
+    employment: ''
   });
 
-  console.log(formInput.comments);
-
   function handleChange(event) {
+    const { name, value, type, checked } = event.target;
     setFormInput((prevFormData) => {
       return {
         ...prevFormData,
-        [event.target.name]: event.target.value
+        [name]: type === 'checkbox' ? checked : value
       };
     });
-  }
+    }
+    
+    console.log(formInput.employment)
 
   return (
     <>
@@ -50,6 +53,53 @@ export default function ControlledInputs() {
         onChange={handleChange}
         placeholder="Comments"
       />
+      <input
+        type="checkbox"
+        id="isFriendly"
+        checked={formInput.isFriendly}
+        onChange={handleChange}
+        name="isFriendly"
+      />
+      <label htmlFor="isFriendly">Are you friendly?</label>
+      <br />
+      <br />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formInput.employment === 'unemployed'}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formInput.employment === 'part-time'}
+          onChange={handleChange}
+        />
+        <label htmlFor="Part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formInput.employment === 'full-time'}
+          onChange={handleChange}
+        />
+        <label htmlFor="fulltime">Full-time</label>
+        <br />
+      </fieldset>
     </>
   );
 }
