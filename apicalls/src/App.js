ba8a7d15ddaf98511d4e86166427ab66a1e21cb2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-26 15:09:37 +0300

    Fetching data from APIs

--- apicalls/src/App.js
+++ apicalls/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 export default function App() {
-  const [starWarsData, setStarWarsData] = React.useState({})
+  const [starWarsData, setStarWarsData] = React.useState({});
+  const [count, setCount] = React.useState(0);
 
-  fetch('https://swapi.dev/api/people/1')
-    .then(res => res.json())
-    .then(data => console.log(data))
+  React.useEffect(() => {
+    console.log('Effect ran');
+    fetch(``)
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+  }, []);
 
   return (
     <div>
+      <h2>The Count is {count}</h2>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
