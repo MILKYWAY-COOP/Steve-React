536882ddc9e258252292b15ea888a2938c38a90b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 07:38:28 +0300

    Added a logo

--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import reactLogo from './images/react-icon-small.svg';
+import reactLogo from './images/logo.png';
 
 export default function Navbar(props) {
   return (
     <nav className={props.darkMode ? 'dark':''}>
-      
+      <img className='nav--logo_icon' src={ reactLogo}/>
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
