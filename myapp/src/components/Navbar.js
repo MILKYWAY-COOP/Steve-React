f6697002182c0e245496d6a995fb005790a29045
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 07:35:20 +0300

    Implementing DarkMode on an app

--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import reactLogo from './images/react-icon-small.svg';
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
-    <nav>
+    <nav className={props.darkMode ? 'dark':''}>
       
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
