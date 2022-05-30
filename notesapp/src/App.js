4ea1707f0b4ef45991b284fc988e021579e64c97
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 10:34:03 +0300

    Notes App

--- notesapp/src/App.js
+++ notesapp/src/App.js
@@ -1,16 +1,22 @@
-import React from "react"
-import { data } from './data';
+import React from 'react';
+// import { data } from './data';
 import Split from 'react-split';
 import { nanoid } from 'nanoid';
 import Sidebar from './components/Sidebar';
-import Editor from './components/Editor'
-
+import Editor from './components/Editor';
 
 export default function App() {
-  const [notes, setNotes] = React.useState([]);
+  
+  const [notes, setNotes] = React.useState(
+    JSON.parse(localStorage.getItem('notes')) || []
+  );
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ''
   );
+
+  React.useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   function createNewNote() {
     const newNote = {
