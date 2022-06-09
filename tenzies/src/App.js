88bc3a3ea68e98345b7bf8372ac8d3b8d083d78b
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-09 13:01:21 +0300

    Dice Elements

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,29 +2,24 @@ import React from 'react';
 import Die from './components/Die';
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice);
+
   function allNewDice() {
     const myList = [];
     for (let i = 0; i < 10; i++) {
       const myNum = Math.floor(Math.random() * 10) + 1;
       myNum > 6 ? myList.push(myNum - 4) : myList.push(myNum);
     }
-    return myList
+    return myList;
   }
+
+  const die = dice.map((item) => {
+    return <Die value={item} />;
+  });
 
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
+      <div className="dice-container">{die}</div>
     </main>
   );
 }
