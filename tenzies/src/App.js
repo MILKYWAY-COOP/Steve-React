0ab185e12ba4fe73628aa655f3ec0efe9413d8c2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-09 12:52:52 +0300

    NewDice

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Die from './components/Die';
 
 function App() {
+  function allNewDice() {
+    const myList = [];
+    for (let i = 0; i < 10; i++) {
+      const myNum = Math.floor(Math.random() * 10) + 1;
+      myNum > 6 ? myList.push(myNum - 4) : myList.push(myNum);
+    }
+    return myList
+  }
+
   return (
-    <main className='main'>
- 
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
     </main>
   );
 }
