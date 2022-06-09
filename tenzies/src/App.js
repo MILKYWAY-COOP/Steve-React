5faa0474fc5f58b89feb15bbf5bb7041045d64bc
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-09 13:30:01 +0300

    Deep React

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,29 +2,33 @@ import React from 'react';
 import Die from './components/Die';
 
 function App() {
-  const [dice, setDice] = React.useState(allNewDice);
+  const [dice, setDice] = React.useState(allNewDice());
 
   function allNewDice() {
     const myList = [];
     for (let i = 0; i < 10; i++) {
       const myNum = Math.floor(Math.random() * 10) + 1;
-      myNum > 6 ? myList.push(myNum - 4) : myList.push(myNum);
+      myNum > 6
+        ? myList.push({ value: myNum - 4, isHeld: false })
+        : myList.push({ value: myNum, isHeld: false });
     }
     return myList;
   }
 
   function rollDice() {
-    setDice(allNewDice)
+    setDice(allNewDice());
   }
 
   const die = dice.map((item) => {
-    return <Die value={item} />;
+    return <Die value={item.value} />;
   });
 
   return (
     <main>
       <div className="dice-container">{die}</div>
-      <button className='roll-btn' onClick={rollDice}>Roll</button>
+      <button className="roll-btn" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
