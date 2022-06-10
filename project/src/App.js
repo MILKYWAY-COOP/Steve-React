80cc75caa18e009859a88c7501416ae4acbdbe4e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-10 12:05:37 +0300

    Working with APIs

--- project/src/App.js
+++ project/src/App.js
@@ -3,23 +3,27 @@ import './index.css';
 
 export default function App() {
   const [movie, setMovie] = React.useState({});
-  const [place, setPlace] = React.useState('');
+  const [location, setLocation] = React.useState('');
+  const [image, setImage] = React.useState('')
 
   React.useEffect(() => {
     console.log('Effect ran');
     fetch(`https://imdb-api.com/en/API/Title/k_1234567/tt1832382`)
       .then((res) => res.json())
       .then((data) => setMovie(data));
-  }, [place]);
+  }, [location]);
 
-  console.log(movie.title);
+  function handleChange(event) {
+    setLocation(event.target.value)
+  }
 
+  console.log(location)
   return (
     <div className="App">
-      <form className="form--form">
+      <div className="form">
         <label for="place">Tell us where you want to go..</label>
-        <input type="text" name="place" />
-      </form>
+        <input type="text" name="place" onChange={handleChange} placeholder="place"/>
+      </div>
     </div>
   );
 }
