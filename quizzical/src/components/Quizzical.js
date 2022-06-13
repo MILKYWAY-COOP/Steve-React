2a8a10f7278384c524a5f65c3537e6dbdaa47b6f
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-13 19:50:25 +0300

    Styles

--- quizzical/src/components/Quizzical.js
+++ quizzical/src/components/Quizzical.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from './styles/quizzical.module.css'
 
 export default function Quizzical() {
     return (
-      <div className={styles.quizzical}>
-        <div className={styles.topRight}></div>
-        <h1 className={styles.h1}>Quizzical</h1>
+      <div className="quizzical">
+        <div className="topRight"></div>
+        <h1 className="h1">Quizzical</h1>
         <h4 className="h4">Come have fun</h4>
         <button className="button">Start Quiz</button>
       </div>
