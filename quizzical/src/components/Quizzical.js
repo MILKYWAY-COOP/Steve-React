e28a930057d96008d123c9512493acb3f403cebd
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-13 20:20:22 +0300

    More Styles

--- quizzical/src/components/Quizzical.js
+++ quizzical/src/components/Quizzical.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 export default function Quizzical() {
-    return (
+  return (
+    <>
+      <div className="topRight"></div>
       <div className="quizzical">
-        <div className="topRight"></div>
         <h1 className="h1">Quizzical</h1>
         <h4 className="h4">Come have fun</h4>
         <button className="button">Start Quiz</button>
       </div>
-    );
+      <div className="bottomLeft"></div>
+    </>
+  );
 }
