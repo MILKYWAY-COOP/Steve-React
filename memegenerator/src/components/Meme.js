f78988715076611bb8256ec73e2ad63483b372e4
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-14 12:38:49 +0300

    Working with data

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -13,6 +13,7 @@ export default function Meme() {
     async function getMemes() {
       const res = await fetch('https://api.imgflip.com/get_memes')
       const data = await res.json()
+      console.log(data)
       setAllMemeImages(data.data.memes)
     }
 
