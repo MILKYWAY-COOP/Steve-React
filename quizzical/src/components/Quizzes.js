1dc2708b917673d04dcc31e6a00bf28838590f57
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-15 15:30:12 +0300

    sorting

--- quizzical/src/components/Quizzes.js
+++ quizzical/src/components/Quizzes.js
@@ -1,13 +1,18 @@
 export default function Quiz(props) {
-  const { question, difficulty, correctAnswer } = props;
-  console.log(props.incorrectAnswers);
+  const { question, difficulty, correctAnswer, incorrectAnswers } = props;
+  const answers = [correctAnswer, ...incorrectAnswers];
+  const shuffledAnswers = answers.sort((a, b) => 0.5 - Math.random());
+
+  const ans = answers.map((answer, index) => {
+    return <div key={index}>{shuffledAnswers}</div>;
+  });
 
   return (
     <>
       <div className="quiz">
-        <div className="question">{}</div>
+        <div className="question">{question}</div>
         <div className="difficulty">{difficulty}</div>
-        <div className="answers"></div>
+        <div className="answers">{ans}</div>
       </div>
     </>
   );
