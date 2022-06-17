1b9be3b3b634bae603c8ce13a4e29b2c784457ba
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-17 21:57:50 +0300

    Updated context

--- context/src/App.js
+++ context/src/App.js
@@ -3,16 +3,17 @@ import Products from './components/Products';
 import Checkout from './components/Checkout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Products />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
