1b75988dbe1314fbb85fa5a24b1f003ef6e46471
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-17 21:58:17 +0300

    Updated context

--- context/src/index.js
+++ context/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {CartProvider} from './cartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
+  <CartProvider>
     <App />
-  </>
+  </CartProvider>
 );
