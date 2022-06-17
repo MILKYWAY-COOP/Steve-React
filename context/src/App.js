64bd5bb3e5f331d0114bdf444f2a88e942f6fd88
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-17 17:43:30 +0300

    Routers

--- context/src/App.js
+++ context/src/App.js
@@ -1,10 +1,20 @@
+import Nav from './components/Nav';
+import Products from './components/Products';
+import CheckOut from './components/CheckOut';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This works</h1>
+    <div className='App'>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Products />} />
+          <Route path='/checkout' element={<CheckOut />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
