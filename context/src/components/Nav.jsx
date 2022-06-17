c6c44902b0c92d29af0ab5c75ca3a00001af7830
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-17 21:59:10 +0300

    Updated UserContext

--- context/src/components/Nav.jsx
+++ context/src/components/Nav.jsx
@@ -1,7 +1,13 @@
 import { AiFillShopping } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import cartContext from '../cartContext';
+import { useContext } from 'react';
+import { useApp} from '../cartContext'
 
 function Nav() {
+  const { item } = useApp();
+  console.log(item);
+
   return (
     <nav>
       <Link to={'/'}>
@@ -10,7 +16,7 @@ function Nav() {
       <Link to={'/checkout'}>
         <div className="cart">
           <AiFillShopping />
-          <span>0</span>
+          <span>{item}</span>
         </div>
       </Link>
     </nav>
