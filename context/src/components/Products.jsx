ba824a70826cce9529b50055db502f5cdc37e6a0
Stephen Muchendu <muchendumaina66@gmail.com>
2022-06-17 18:09:49 +0300

    Products

---
+++ context/src/components/Products.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Card from './Card';
+
+function Products() {
+  const products = [
+    { name: 'vest', price: 100 },
+    { name: 't-shirt', price: 50 },
+    { name: 'jacket', price: 200 },
+    { name: 'shoes', price: 150 },
+    { name: 'socks', price: 50 },
+    { name: 'gloves', price: 100 },
+    { name: 'hat', price: 50 },
+    { name: 'jacket', price: 200 }
+  ];
+}
