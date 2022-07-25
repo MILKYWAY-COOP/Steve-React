1e033d9f30ea7c01f765a5d4411f6abe4bcf0052
Stephen Muchendu <muchendumaina66@gmail.com>
2022-07-25 05:43:05 +0300

    Added a NavBar

--- mmgen/src/App.js
+++ mmgen/src/App.js
@@ -1,8 +1,10 @@
 import Meme from './components/Meme';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <Meme />
     </div>
   );
