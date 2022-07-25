535072fe67f533171d744b16a6aefd699e79a7e2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-07-25 09:22:02 +0300

    Radio & Check Boxes

--- frms/src/components/Form.js
+++ frms/src/components/Form.js
@@ -6,21 +6,22 @@ export default function Form() {
     lastName: '',
     email: '',
     comments: '',
-    isFriendly: true
+    isFriendly: true,
+    employment: ''
   });
 
-  console.log(formData.comments);
+  console.log(formData.employment);
 
   function handleChange(event) {
     //destructure the event.target.name and event.target.value
-      const { name, value, type, checked } = event.target;
-      setFormData(prevState => {
-          return {
-              ...prevState,
-              [name]: type === 'checkbox' ? checked : value
-          }
-      });
-    }
+    const { name, value, type, checked } = event.target;
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: type === 'checkbox' ? checked : value
+      };
+    });
+  }
 
   //check whether email is valid
   const { email } = formData;
@@ -78,6 +79,44 @@ export default function Form() {
         onClick={handleChange}
       />
       <label htmlFor="isFriendly">Check me out</label>
+
+      {/* Add a radio button */}
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          onChange={handleChange}
+          checked={formData.employment === 'unemployed'}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          onChange={handleChange}
+          checked={formData.employment === 'part-time'}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          onChange={handleChange}
+          checked={formData.employment === 'full-time'}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+      </fieldset>
     </form>
   );
 }
