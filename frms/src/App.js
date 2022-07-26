d7954e2d5d2e08f969a9772f476c99db22ecef9f
Stephen Muchendu <muchendumaina66@gmail.com>
2022-07-26 12:29:18 +0300

    Forms Recap

--- frms/src/App.js
+++ frms/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Form from './components/Form';
+import Login from './components/Pass';
 
 function App() {
   return (
     <>
-      <Form />
+      <Login />
     </>
   );
 }
