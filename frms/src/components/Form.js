6082d86ea1dcd427b1c73a9522d3d77a1fbc41d7
Stephen Muchendu <muchendumaina66@gmail.com>
2022-07-26 11:48:55 +0300

    Form Recap

--- frms/src/components/Form.js
+++ frms/src/components/Form.js
@@ -7,10 +7,9 @@ export default function Form() {
     email: '',
     comments: '',
     isFriendly: true,
-    employment: ''
+    employment: '',
+    favColor: ''
   });
-
-  console.log(formData.employment);
 
   function handleChange(event) {
     //destructure the event.target.name and event.target.value
@@ -35,88 +34,114 @@ export default function Form() {
     }
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    validateEmail(email);
+    console.log(formData);
+  }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
-        type="text"
-        placeholder="First Name"
+        type='text'
+        placeholder='First Name'
         onChange={handleChange}
-        name="firstName"
+        name='firstName'
         value={formData.firstName}
       />
       <input
-        type="text"
-        placeholder="Last Name"
+        type='text'
+        placeholder='Last Name'
         onChange={handleChange}
-        name="lastName"
+        name='lastName'
         value={formData.lastName}
       />
       <input
-        type="email"
-        placeholder="Email"
+        type='email'
+        placeholder='Email'
         onChange={handleChange}
-        name="email"
+        name='email'
         value={formData.email}
       />
-      <button type="submit" onClick={() => validateEmail(email)}>
-        Submit
-      </button>
 
       {/* add a text area to the form */}
       <textarea
-        placeholder="Enter your comments here"
+        placeholder='Enter your comments here'
         onChange={handleChange}
         value={formData.comments}
-        name="comments"
+        name='comments'
       />
 
       {/* Add a check box */}
       <input
-        type="checkbox"
-        name="isFriendly"
-        id="isFriendly"
+        type='checkbox'
+        name='isFriendly'
+        id='isFriendly'
         checked={formData.isFriendly}
         onClick={handleChange}
       />
-      <label htmlFor="isFriendly">Check me out</label>
+      <label htmlFor='isFriendly'>Check me out</label>
 
       {/* Add a radio button */}
       <fieldset>
         <legend>Current employment status</legend>
 
         <input
-          type="radio"
-          id="unemployed"
-          name="employment"
-          value="unemployed"
+          type='radio'
+          id='unemployed'
+          name='employment'
+          value='unemployed'
           onChange={handleChange}
           checked={formData.employment === 'unemployed'}
         />
-        <label htmlFor="unemployed">Unemployed</label>
+        <label htmlFor='unemployed'>Unemployed</label>
         <br />
 
         <input
-          type="radio"
-          id="part-time"
-          name="employment"
-          value="part-time"
+          type='radio'
+          id='part-time'
+          name='employment'
+          value='part-time'
           onChange={handleChange}
           checked={formData.employment === 'part-time'}
         />
-        <label htmlFor="part-time">Part-time</label>
+        <label htmlFor='part-time'>Part-time</label>
         <br />
 
         <input
-          type="radio"
-          id="full-time"
-          name="employment"
-          value="full-time"
+          type='radio'
+          id='full-time'
+          name='employment'
+          value='full-time'
           onChange={handleChange}
           checked={formData.employment === 'full-time'}
         />
-        <label htmlFor="full-time">Full-time</label>
+        <label htmlFor='full-time'>Full-time</label>
         <br />
       </fieldset>
+
+      {/* Create a select box */}
+      <select
+        id='favColor'
+        value={formData.favColor}
+        onChange={handleChange}
+        name='favColor'
+      >
+        <option value=''>-- Choose --</option>
+        <option value='red'>Red</option>
+        <option value='orange'>Orange</option>
+        <option value='yellow'>Yellow</option>
+        <option value='green'>Green</option>
+        <option value='blue'>Blue</option>
+        <option value='indigo'>Indigo</option>
+        <option value='violet'>Violet</option>
+      </select>
+
+      {/* Add a submit button */}
+      <br/>
+      <br/>
+      <button>Submit</button>
     </form>
   );
 }
